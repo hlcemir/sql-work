4da13c7f8669ef0a15cf4e9e7571d196d3f00aed
hlcemir <105809112+hlcemir@users.noreply.github.com>
2022-08-04 11:19:02 +0300

    Add files via upload

---
+++ sql-work7.sql
@@ -0,0 +1,21 @@
+--1
+SELECT rating FROM film
+GROUP BY rating;
+
+--2
+SELECT replacement_cost, COUNT(*) 
+FROM film 
+GROUP BY replacement_cost
+HAVING count(replacement_cost) >50
+
+--3
+SELECT store_id, COUNT(*) 
+FROM customer 
+GROUP BY store_id;
+
+--4
+SELECT country_id, COUNT(city) 
+FROM city 
+GROUP BY country_id
+ORDER BY COUNT(country_id) DESC
+LIMIT 1 ;
